7fb5d4bbc9186ee837b5ceb9ea5865d2b0bb0732
ashgolan <ashgolan@hotmail.com>
2023-01-04 12:40:55 +0200

    fix import sendmail

--- server/emails/account.js
+++ server/emails/account.js
@@ -2,36 +2,20 @@ import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 dotenv.config();
 
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// export const sendMail = (mail, name, amount) => {
-//   sgMail
-//     .send({
-//       to: mail,
-//       from: "alaashaalan.test@gmail.com",
-//       subject: `hello ${name}`,
-//       text: "reminder",
-//       html: `<strong>היתרה העדכנית בחנות אלריחאן היא :  ${amount}</strong>`,
-//     })
-//     .then(() => {
-//       console.log("Email sent");
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "alaa.t.shaalan@gmail.com", // Change to your recipient
-  from: "alaa.t.shaalan@gmail.com", // Change to your verified sender
-  subject: "new",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+export const sendMail = (mail, name, amount) => {
+  sgMail
+    .send({
+      to: mail,
+      from: "alaa.t.shaalan@gmail.com",
+      subject: `hello ${name}`,
+      text: "reminder",
+      html: `<strong>היתרה העדכנית בחנות אלריחאן היא :  ${amount}</strong>`,
+    })
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
