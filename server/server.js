3afc32c46cf77d9d01e9aa69aaf3c17bb11fe5d1
ashgolan <ashgolan@hotmail.com>
2023-01-04 13:11:44 +0200

    seession fix

--- server/server.js
+++ server/server.js
@@ -32,7 +32,27 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.set("trust proxy", 1);
 
+app.use(
+  session({
+    cookie: {
+      secure: true,
+      maxAge: 60000,
+    },
+    store: new RedisStore(),
+    secret: "secret",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use(function (req, res, next) {
+  if (!req.session) {
+    return next(new Error("Oh no")); //handle error
+  }
+  next(); //otherwise continue
+});
 app.get("/", getAllData);
 app.use("/clients", clientRouter);
 app.use("/transactions", transactionRouter);
